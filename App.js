15a0fe39f354f802348b439f07f3c3fb9544f30e
poojabare <146422109+poojabare@users.noreply.github.com>
2024-02-10 22:50:12 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,13 +1,33 @@
-import React from 'react';
-import Registration from './Componts/Registration';
+import logo from './logo.svg';
+import './App.css';
+import ViewHome from './Pages/ViewHome';
+import Registration from './Pages/Registration';
+import Login from './Pages/Login';
+import { BrowserRouter as Router, Route, Routes, BrowserRouter } from "react-router-dom";
+import HomePage from './Pages/HomePage';
 
 function App() {
   return (
-    <div>
-      
-      <Registration/>
-    </div>
+<>
+<Router>
+
+      <Routes>
+        <Route path='' exact element={<ViewHome/>} />
+        <Route path='/register' element={<Registration/>} />
+        <Route path='/Login' exact element={<Login/>} />
+        <Route path='/home' element={<HomePage/>} /> 
+        
+      </Routes>
+
+
+
+    
+</Router>
+
+    
+    </>
   );
 }
 
 export default App;
+
